83f0b6683d415ee4280cbfcf8f5d958341f77273
zhoudr <zdianrui@126.com>
2016-12-13 23:12:17 +0800

    update

--- src/main/java/io/vicp/frlib/micromsg/web/listener/ApplicationListener.java
+++ src/main/java/io/vicp/frlib/micromsg/web/listener/ApplicationListener.java
@@ -1,6 +1,7 @@
 package io.vicp.frlib.micromsg.web.listener;
 
 import io.vicp.frlib.micromsg.util.ResourceConfigUtil;
+import org.springframework.web.context.ContextLoaderListener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -8,14 +9,16 @@ import javax.servlet.ServletContextListener;
 /**
  * Created by zhoudr on 2016/12/12.
  */
-public class ApplicationListener implements ServletContextListener {
+public class ApplicationListener extends ContextLoaderListener {
 
     public void contextInitialized(ServletContextEvent sce) {
+        super.contextInitialized(sce);
         String env = ResourceConfigUtil.getProperty("env");
         System.out.println("application initialized, current system is " + env);
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
         System.out.println("application destroyed");
+        super.contextDestroyed(sce);
     }
 }
